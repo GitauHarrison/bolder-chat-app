e9f632e545af24ab6a030aa6d9910499c0e81842
Gitau Harrison <harrisongitau3@gmail.com>
2020-11-02 08:45:44 +0300

    Handle user login sessions

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_bootstrap import Bootstrap
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -10,6 +11,7 @@ app.config.from_object(Config)
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
 
 from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import render_template, url_for, flash, redirect
 from app.forms import LoginForm
+from app.models import User
+from flask_login import login_user, current_user, logout_user
 
 @app.route('/')
 @app.route('/home')
@@ -9,12 +11,15 @@ def home():
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
+        user = User.query.filter_by(username = form.username.data).first()
+        if user is None or user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember = form.remember_me.data)
         flash('You have successfully logged in!')
         return redirect(url_for('home'))
     return render_template('login.html', title = 'Login', form = form)
-
-@app.route('/register')
-def register():
-    return render_template ('register.html', title = 'Register')
